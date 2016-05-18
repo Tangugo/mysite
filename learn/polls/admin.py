58d48b703355e626f86d8b75ec00cd4d4021a7af
zwb <zwb35.gmail.com>
2016-05-18 18:11:14 +0800

    modify 1

---
+++ learn/polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ('Question information', {'fields': ['question_text']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+
+admin.site.register(Question, QuestionAdmin)
---
+++ learn/polls/models.py
@@ -0,0 +1,39 @@
+#!/usr/local/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200, help_text=u'问题描述')
+    pub_date = models.DateTimeField('date published')
+
+    class Meta():
+        db_table = 'tb_questions'
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'published recently?'
+
+    def __str__(self):
+        return self.question_text
+
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    class Meta():
+        db_table = 'tb_choice'
+
+    def __str__(self):
+        return self.choice_text
